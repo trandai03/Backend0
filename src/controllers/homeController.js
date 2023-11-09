b313720fc2bd76a53ff9b3537d61185530ecff4c
trandai03 <21011589@st.phenikaa-uni.edu.vn>
2023-11-09 11:50:37 +0700

    query list user

--- src/controllers/homeController.js
+++ src/controllers/homeController.js
@@ -1,5 +1,14 @@
-const getHomepage = (req,res) => {
-    res.send('Hello World! Tran Dai')
+const connection = require('../config/database')
+const { getAllUsers } = require('../services/CRUDServices')
+
+
+const getHomepage = async (req,res) => {
+    let results = await getAllUsers();
+    return res.render('home.ejs',{listUsers : results})
+}
+
+const getCreatePage = (req,res) => {
+    return res.render('create.ejs')
 
 }
 
@@ -12,6 +21,15 @@ const abc = (req,res) => {
     res.render('sample.ejs')
 }
 
+const postCreateUser = async (req,res) => {
+    let {email,myName ,city} = req.body;
+
+    let [result, fields] = await connection.query(
+            `INSERT INTO  Users (email ,name , city) VALUES (? ,? ,?)`, [email, myName, city]);
+    res.send('create user success')
+}
+
+
 module.exports = {
-    getHomepage,checkABC,abc
+    getHomepage,checkABC,abc,postCreateUser,getCreatePage
 }
--- src/routers/web.js
+++ src/routers/web.js
@@ -1,11 +1,12 @@
 const express = require('express');
 const router = express.Router();
-const { getHomepage, checkABC ,abc} = require('../controllers/homeController');
+const { getHomepage, checkABC ,abc,postCreateUser,getCreatePage} = require('../controllers/homeController');
 
 // router.Method('/route', handler)
 router.get('/' , getHomepage);
 router.get('/check' , checkABC);
-
 router.get('/abc', abc)
+router.get('/create', getCreatePage)
 
+router.post('/create-user', postCreateUser)
 module.exports = router //export default 
